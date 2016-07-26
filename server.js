85ed99186db665380a73538939ad6749fca606f5
Ahmed Noman <ahmed.alnoman@gmail.com>
2016-07-26 15:19:31 -0400

    Add PUT todos

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.get('/', function (request, response) {
 app.get('/todos', function (request, response) {
     var queryParams = request.query;
     var filteredTodos = todos;
-    
+
     if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'true' ) {
         filteredTodos = _.where(filteredTodos, {completed: true});
     } else if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'false') {
@@ -28,7 +28,7 @@ app.get('/todos', function (request, response) {
         filteredTodos = _.filter(filteredTodos, function (todo) {
             return todo.description.toLowerCase().indexOf(queryParams.description.toLowerCase()) > -1;
         });
-    } 
+    }
 
     response.json(filteredTodos);
 });
@@ -50,7 +50,7 @@ app.post('/todos', function (request, response) {
     var body = _.pick(request.body, 'description', 'completed') ;
 
     if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
-        return response.status(400).send();      
+        return response.status(400).send();
     }
 
     body.description = body.description.trim();
@@ -72,9 +72,40 @@ app.delete('/todos/:id', function (request, response) {
     if (!matchedTodo) {
         response.status(404).json({"error": "no todo found with that id"});
     } else {
-        todos =  _.without(todos, matchedTodo)  
+        todos =  _.without(todos, matchedTodo)
         response.json(todos);
     }
+});
+
+// PUT /todos/:id
+app.put('/todos/:id', function (request, response) {
+    var todoID = parseInt(request.params.id, 10);
+    var matchedTodo = _.findWhere(todos, {id: todoID});
+    var body = _.pick(request.body, 'description', 'completed');
+    var validAttributes = {};
+
+    if (!matchedTodo) {
+        return response.status(404).send();
+    }
+
+    if (body.hasOwnProperty('completed') && _.isBoolean(body.completed)) {
+        validAttributes.completed = body.completed;
+    } else if (body.hasOwnProperty('completed')) {
+        return response.status(400).send();
+    } else {
+        // Never provided attribute, no problem here
+    }
+
+    if (body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim() > 0) {
+        validAttributes.description = body.description;
+    } else if (body.hasOwnProperty('description')) {
+        return response.status(400).send();
+    } else {
+        // Never provided attribute, no problem here
+    }
+
+    _.extend(matchedTodo, validAttributes);
+    response.json(matchedTodo);
 });
 
 app.listen(PORT, function () {
